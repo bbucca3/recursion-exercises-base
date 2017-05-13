46fe823c3a695f291e7d913fc2983769658e2021
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-12 19:31:46 -0700

    Implement jsoninator tests

--- jsoninator.js
+++ jsoninator.js
@@ -8,6 +8,30 @@ const _ = require('underscore'); // the real one! :)
 
 const stringify = function(obj) {
   // your code goes here
+  switch (typeof obj) {
+    case ('null'): return null;
+    case ('number'): return obj.toString();
+    case ('string'): return '\"' + obj + '\"';
+    case ('object'): 
+      if (Array.isArray(obj)) {
+        let result = '[';
+        for (let i = 0; i < obj.length; i++) {
+          if (i > 0) {
+            result += ',';
+          }
+          result += stringify(obj[i]);
+        }
+        return result + ']';
+      }
+      else {
+        let result = '{';
+        for (let key in obj) {
+          result += stringify(key) + ':' + stringify(obj[key]);
+        }
+        return result + '}';
+      }
+    default: return String(obj);  
+  }
 };
 
 module.exports = {
