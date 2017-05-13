6591f133cd804c816b371ae310558f41b529ab04
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-12 17:35:33 -0700

    Implement dom-util tests

--- dom-util.js
+++ dom-util.js
@@ -2,11 +2,19 @@
 const visitAllNodes = function(node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
   // Your code here
+  callback(node);
+  let child = node.childNodes;
+  if (child.length > 0) {
+    child.forEach(element => visitAllNodes(element, callback));
+  }
 };
 
 const flattenTreeToArray = function(node) {
   // Hint: Use visitAllNodes()
   // Your code here
+  let flatArray = [];
+  visitAllNodes(node, element => flatArray.push(element));
+  return flatArray;
 };
 
 module.exports = {
