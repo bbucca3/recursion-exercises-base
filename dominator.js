08ea1c044a1713c77c7fe4fcdfbbf89393044c48
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-12 22:02:52 -0700

    Implement dominator functions for tests

--- dominator.js
+++ dominator.js
@@ -3,14 +3,17 @@ const _ = require('underscore');
 
 const getElementById = function(root, id) {
   // Your code here
+  return _.filter(flattenTreeToArray(root), element => !!element.id && element.id === id)[0] || null;
 };
 
 const getElementsByClassName = function(root, className) {
   // Your code here
+  return _.filter(flattenTreeToArray(root), element => !!element.className && element.className.includes(className));
 };
 
 const getElementsByTagName = function(root, tagName) {
   // Your code here
+  return _.filter(flattenTreeToArray(root), element => !!element.tagName && element.tagName.includes(tagName));
 };
 
 module.exports = {
